e4083977f8980c72ec8aa47d34ce1c0814314e50
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-07-25 13:18:34 +0530

    Update 03_objects.js(singleton, symbol adding & accessing it, backtick)

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,15 +1,18 @@
-// singleton
-// Object.create
+// singleton :- when we create a obj through constructor(Object.create) then this object is the only one object
+
+//there are 2 ways to create Objects :- 1. obj constructor &    2. Obj ltereal
+
+// Object.create -> obj constructor -> singleton
 
 // object literals
 
-const mySym = Symbol("key1")
+const mySym = Symbol("key1") //Interview question => how to add the symbol in any object & access it? 
 
 
 const JsUser = {
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    [mySym]: "mykey1",   //ans1 -> adding the symbol in object
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -20,7 +23,7 @@ const JsUser = {
 // console.log(JsUser.email)
 // console.log(JsUser["email"])
 // console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+// console.log(JsUser[mySym])  //ans2 -> accessing the symbol of object => [Symbol(key1)]: 'myKey1'
 
 JsUser.email = "hitesh@chatgpt.com"
 // Object.freeze(JsUser)
@@ -31,7 +34,7 @@ JsUser.greeting = function(){
     console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+    console.log(`Hello JS user, ${this.name}`); //this backtick(``) is known as string interpolation
 }
 
 console.log(JsUser.greeting());
