32278727ca389852d978cf1d761d2aec905ec057
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-07-25 13:37:27 +0530

    Update 04_objects.js(?, adding 2 or more projects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -18,13 +18,15 @@ const regularUser = {
 }
 
 // console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname?.userfullname.firstname); //here this(?) means when fullname is not exist in the object then skip it(it is also known as optional chaning
 
+//Q -> how to join 2 or more objects?
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = Object.assign({}, obj1, obj2, obj4)  //Object.assign(target, source)
 
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
