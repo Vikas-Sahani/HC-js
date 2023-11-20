edf9c3fbb95c0906fa2a33467040f7f3a8da4296
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-11-20 10:26:42 +0000

    importance of semicolon in iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -4,7 +4,7 @@
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})();  //this(;) semicolon is very important to run the another iife 
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
